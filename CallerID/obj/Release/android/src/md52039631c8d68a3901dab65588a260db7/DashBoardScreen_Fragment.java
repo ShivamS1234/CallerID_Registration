2f600391666e8108b6b66668da7b5f2d35461d41
ShivamS1234 <shivammcu@gmail.com>
2018-12-03 10:56:18 +0530

    add .apk file here

---
+++ CallerID/obj/Release/android/src/md52039631c8d68a3901dab65588a260db7/DashBoardScreen_Fragment.java
@@ -0,0 +1,56 @@
+package md52039631c8d68a3901dab65588a260db7;
+
+
+public class DashBoardScreen_Fragment
+	extends android.app.Fragment
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onCreateView:(Landroid/view/LayoutInflater;Landroid/view/ViewGroup;Landroid/os/Bundle;)Landroid/view/View;:GetOnCreateView_Landroid_view_LayoutInflater_Landroid_view_ViewGroup_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("CallerID.Resources.fragment.DashBoardScreen_Fragment, CallerID", DashBoardScreen_Fragment.class, __md_methods);
+	}
+
+
+	public DashBoardScreen_Fragment ()
+	{
+		super ();
+		if (getClass () == DashBoardScreen_Fragment.class)
+			mono.android.TypeManager.Activate ("CallerID.Resources.fragment.DashBoardScreen_Fragment, CallerID", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public android.view.View onCreateView (android.view.LayoutInflater p0, android.view.ViewGroup p1, android.os.Bundle p2)
+	{
+		return n_onCreateView (p0, p1, p2);
+	}
+
+	private native android.view.View n_onCreateView (android.view.LayoutInflater p0, android.view.ViewGroup p1, android.os.Bundle p2);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
